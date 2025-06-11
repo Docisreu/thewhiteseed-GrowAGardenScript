730ff624b8076e4eb7ed13e9cbedd7485072b46c
Docisreu <fredericklael.unabia@gmail.com>
2025-06-11 14:35:41 +0800

    Update GrowAGardenLoader.lua

--- GrowAGardenLoader.lua
+++ GrowAGardenLoader.lua
@@ -1,2 +1,2 @@
-local mainScript = game:HttpGet("https://raw.githubusercontent.com/thewhitesee/GrowAGardenScript/main/GrowAGardenMain.lua")
+local mainScript = game:HttpGet("https://raw.githubusercontent.com/thewhiteseed/GrowAGardenScript/main/GrowAGardenMain.lua")
 loadstring(mainScript)()
